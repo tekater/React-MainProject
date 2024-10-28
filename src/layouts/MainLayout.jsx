6e03bc25be6bd80f8266faf44a8fd93614af9ef9
tekater <tekateplay1@yandex.ru>
2024-10-28 19:07:56 +0900

    Routes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow container mx-auto p-4 mt-16">
+      <main className="flex-grow mx-auto p-4 mt-16">
         <Outlet />
       </main>
       <Footer />
@@ -14,4 +14,4 @@ const MainLayout = () => {
   );
 };
 
-export default MainLayout;
+export default MainLayout;z
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,54 +1,35 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import Cards from "../pages/Cards";
 import CardDetails from "../pages/CardDetails";
-import Cart from "../pages/Cart";
-import Forbidden from "../pages/Forbidden";
-import PrivateRoute from "../routes/PrivateRoute";
-import { useAuth } from "../hooks/useAuth";
-import Checkout from "../pages/Checkout";
-import Admin from "../pages/Admin";
 
-/** Массив роутов приложения */
+/**Роуты приложения */
 const routes = [
-  { path: "/home", element: <PrivateRoute element={<Home />} /> },
-  { path: "cards", element: <PrivateRoute element={<Cards />} /> },
-  { path: "cards/:alias", element: <PrivateRoute element={<CardDetails />} /> },
-  { path: "cart", element: <PrivateRoute element={<Cart />} /> },
-  { path: "/cart/checkout", element: <PrivateRoute element={<Checkout />} /> },
-  {
-    path: "admin",
-    element: <PrivateRoute element={<Admin />} requiredRole="admin" />,
-  },
-  { path: "/forbidden", element: <Forbidden /> },
+    { path: "/", element: <Home /> },
+    { path: "cards", element: <Cards /> },
+    { path: "cards/:alias", element: <CardDetails /> },
 ];
 
 /**
  * Рекурсивно отображает роуты и дочерние роуты.
- * @param {Array} routes - Массив объектов роута.
+ * @param {RouteItem} route - Объект роута.
  * @returns {JSX.Element} JSX элемент роута.
  */
 const renderRoute = ({ path, element, children }) => (
-  <Route key={path} path={path} element={element}>
-    {children && children?.map(renderRoute)}
-  </Route>
+    <Route key={path} path={path} element={element}>
+        {children && children.map(renderRoute)}
+    </Route>
 );
 
 /** Корневой компонент приложения с роутами */
-const AppRoutes = () => {
-  // Хук для проверки авторизации
-  const { user } = useAuth();
-
-  return (
+const AppRoutes = () => (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        {user && <Route path="/" element={<Navigate to="/home" replace />} />}
-        {routes?.map(renderRoute)}
-      </Route>
+        <Route path="/" element={<MainLayout />}>
+            {routes?.map(renderRoute)}
+        </Route>
     </Routes>
-  );
-};
+);
 
 export default AppRoutes;
