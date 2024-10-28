b521e55e3ddaf4e9b523c9e2578bff9f941af930
tekater <tekateplay1@yandex.ru>
2024-10-28 18:51:22 +0900

    Card

--- src/components/ui/Card/Card.jsx
+++ src/components/ui/Card/Card.jsx
@@ -12,25 +12,26 @@ import styles from"./Card.module.css";
 * @param {string} props.imgSrc - Путь к изображению
 * @return {JSX.Element} - Элемент JSX
 */
-const Card = (props) => {const { name, category, description, price, rating, imgSrc, isFavorite } = props.details;
-const { onCardClick } = props;
+const Card = (props) => {
+    const { name, category, description, price, rating, imgSrc, isFavorite } = props.details;
+    const { onCardClick } = props;
     return (
-    <div onClick={onCardClick} id="card" className={`${styles.flex} ${styles.card} ${styles.pay}`}>
-        <div className={isFavorite ? styles.featured_isCheck : styles.featured}>Featured</div>
-        {imgSrc && <img src = {imgSrc} alt = {name} />}
-        <div className={styles.p_20}>
-            {name && <h2 className={styles.text_font_24}>{name}</h2>}
-            <div className={styles.color_text_gray}>
-                {description && <p>{description}</p>}        
-                {category && <p>{category}</p>}        
-                {rating && <span>⭐{rating}</span>}                
+        <div onClick={onCardClick} id="card" className={`${styles.flex} ${styles.card} ${styles.pay}`}>
+            <div className={isFavorite ? styles.featured_isCheck : styles.featured}>Featured</div>
+            {imgSrc && <img src={imgSrc} alt={name} />}
+            <div className={styles.p_20}>
+                {name && <h2 className={styles.text_font_24}>{name}</h2>}
+                <div className={styles.color_text_gray}>
+                    {description && <p>{description}</p>}
+                    {category && <p>{category}</p>}
+                    {rating && <span>⭐{rating}</span>}
+                </div>
+                <div className={`${styles.flex} ${styles.pt_20}`}>
+                    {price && <span className={styles.text_font_24}><b>${price}</b></span>}
+                    <button className={styles.button} type="button"> Buy now </button>
+                </div>
             </div>
-            <div className={`${styles.flex} ${styles.pt_20}` }>
-                {price && <span className={styles.text_font_24}><b>${price}</b></span>}
-                <button className={styles.button} type ="button"> Details </button>            
-            </div>          
         </div>
-    </div>
     );
 };
 
--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,52 +1,36 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Card from "../components/ui/Card/Card";
+import { data } from "../../data";
 import { useNavigate } from "react-router-dom";
-import useProducts from "../store/useProducts";
-import { useAuth } from "../hooks/useAuth";
-import RenderList from "../utils/renderList";
 
 const Cards = () => {
-  // Достаем из стора продукты и метод получения данных
-  const { products } = useProducts();
+    const navigate = useNavigate();
 
-  // Достаем пользователя из кастомного хука
-  const { user } = useAuth();
+    /**
+     * Обработчик клика по карточке.
+     * @param {object} data - Данные карточки.
+     */
+    const handleClick = (data) => {
+        navigate(`/cards/${data?.alias}`, { state: data });
+    };
 
-  // Фильтрация товаров по userId
-  const filteredProducts = products?.filter(product => product?.userId === user?.id);
-
-  // хук react-router-dom для навигации по страницам
-  const navigate = useNavigate();
-
-  /**
-   * Обработчик клика по карточке.
-   * @param {object} cardData - Данные карточки.
-   */
-  const handleClick = (cardData) => {
-    navigate(`/cards/${cardData?.alias}`, { state: cardData });
-  };
-
-  return (
-    <main id="app" className="py-8">
-      <div className="max-w-7xl mx-auto px-2">
-        <h2 className="text-3xl font-bold">Cards page</h2>
-        <div className="flex flex-wrap justify-between">
-          {filteredProducts && (
-            <RenderList
-              items={filteredProducts}
-              render={(productInfo) => (
-                <Card
-                  details={productInfo}
-                  onCardClick={() => handleClick(productInfo)}
-                />
-              )}
-              emptyState={<p>No products found</p>}
-            />
-          )}
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main id="app" >
+            <div className="max-w-7xl mx-auto px-2">
+                <div className="flex flex-wrap justify-between">
+                    {data?.length > 0 &&
+                        data?.map((productInfo) => {
+                            return (
+                                <Card onCardClick={() => handleClick(productInfo)}
+                                    key={productInfo?.id}
+                                    details={productInfo}
+                                />
+                            );
+                        })}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Cards;
